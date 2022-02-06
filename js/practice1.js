89b0eea737f5cf244daf431c9715ba07989ae380
filin_sleep <keepwannadie@gmail.com>
2022-02-06 16:41:58 +0300

    fix

--- js/practice1.js
+++ js/practice1.js
@@ -14,7 +14,7 @@ for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ''),
         b = prompt("На сколько оцените его?", '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b < 10) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b < 11) {
         personalMovieDB.movies[a] = b;
         console.log('done');
     } else {
@@ -25,7 +25,7 @@ for (let i = 0; i < 2; i++) {
 
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов.");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
     console.log("Вы классический зритель.");
 } else if (personalMovieDB.count >= 30) {
     console.log("Вы киноман.");
