bc6c93f43672c1b913ff14119813dcb4f92311ca
filin_sleep <keepwannadie@gmail.com>
2022-02-06 17:35:21 +0300

    functions

---
+++ js/practice2.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+}
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+
+function ret() {
+    let num = 50;
+
+    //
+
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
